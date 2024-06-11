6e48b548f80bc33c0ca8891813b81edd7281aa23
James Solomon-Rounce <James-SR@users.noreply.github.com>
2024-06-11 07:23:18 +0100

    Update HRWeatherStation.sql
    Completed end of directed learning and employee queries

--- HRWeatherStation.sql
+++ HRWeatherStation.sql
@@ -7,14 +7,21 @@ Use a Regex statement:
 
 # Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates
 
-SELECT DISTINCT CITY
-FROM STATION
-WHERE CITY NOT LIKE '[aeiou]%' # or if ending in a vowel WHERE CITY NOT LIKE '%[aeiou]'
+  SELECT DISTINCT CITY
+  FROM STATION
+  WHERE CITY NOT LIKE '[aeiou]%' # or if ending in a vowel WHERE CITY NOT LIKE '%[aeiou]'
 
 # Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
 # Note to handle upper and lower case letters, you may need to use these in the pattern
 
- SELECT DISTINCT CITY
+  SELECT DISTINCT CITY
   FROM STATION
   WHERE CITY NOT LIKE '[aeiouAEIOU]%' AND CITY NOT LIKE '%[aeiouAEIOU]'
  
+# Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+
+  SELECT Name
+  FROM STUDENTS
+  WHERE Marks >75
+  ORDER BY RIGHT(Name, 3), ID
+
