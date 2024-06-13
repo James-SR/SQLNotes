e61289db9f88784bcbcff88c8f72897b9369c43b
James Solomon-Rounce <James-SR@users.noreply.github.com>
2024-06-13 07:53:10 +0100

    Update AdvancedJoins.sql

--- AdvancedJoins.sql
+++ AdvancedJoins.sql
@@ -3,3 +3,9 @@
   LEFT JOIN Submissions ON Hackers.hacker_id = Hackers.hacker_ID
   GROUP BY Hackers.name, submissions.submission_id
   ORDER BY name
+
+  SELECT s.submission_date, COUNT (DISTINCT s.hacker_ID), h.name, h.hacker_ID
+  FROM submissions s
+  LEFT JOIN hackers h ON h.hacker_id = s.hacker_ID
+  GROUP BY h.name,  h.hacker_ID, s.submission_date
+  ORDER BY s.submission_date ASC
