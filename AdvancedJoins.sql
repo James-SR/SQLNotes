e3b8376c5f312153953d40ef6a348595fdc68fd5
James Solomon-Rounce <James-SR@users.noreply.github.com>
2024-06-18 07:33:47 +0100

    Update AdvancedJoins.sql

--- AdvancedJoins.sql
+++ AdvancedJoins.sql
@@ -22,3 +22,14 @@ SELECT s.submission_date, COUNT(h.name)
     FROM submissions 
     GROUP BY hacker_id, submission_date
     ORDER BY submission_date ASC, mycount DESC
+
+# counts submissions by hacker and by date as a subquery
+
+SELECT  sub.*
+FROM (
+    SELECT submission_date,  s.hacker_id,  h.name, COUNT(s.hacker_id) AS mycount
+    FROM submissions s
+    LEFT JOIN hackers h ON h.hacker_id = s.hacker_ID
+    GROUP BY h.name, s.hacker_id, submission_date
+            ) sub
+ORDER BY submission_date ASC, mycount DESC
