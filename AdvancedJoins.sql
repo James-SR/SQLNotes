46cab92b10bf559aef58db016ee30be040975acd
James Solomon-Rounce <James-SR@users.noreply.github.com>
2024-06-24 05:46:51 +0100

    Update AdvancedJoins.sql

--- AdvancedJoins.sql
+++ AdvancedJoins.sql
@@ -42,3 +42,28 @@ SELECT
 FROM submissions
 GROUP BY submission_date
 ORDER BY submission_date
+
+-- Next we have the number of submissions per day with the top (most) submissions by hacker    
+-- Row Number will allow selecting just the top instance i.e. the hacker with the most submissions
+-- on a date.  The partition says when to start counting again, in this case each day
+--  creates temporary data set whose output is available to be referenced in subsequent queries
+
+WITH max_hacker AS (
+    SELECT 
+        ROW_NUMBER() OVER(PARTITION BY submission_date ORDER BY COUNT(s.hacker_id) DESC, s.hacker_ID ASC) AS Row#,
+        s.hacker_id, 
+        submission_date,
+        name
+    FROM submissions s
+    JOIN hackers h ON h.hacker_id = s.hacker_ID
+    GROUP BY submission_date, s.hacker_id, name
+)
+SELECT
+    s.submission_date,
+    COUNT(DISTINCT s.hacker_ID),
+    mh.hacker_id,
+    mh.name
+FROM submissions s
+JOIN max_hacker mh ON mh.submission_date = s.submission_date AND Row# = 1
+GROUP BY s.submission_date, mh.hacker_id, mh.name
+ORDER BY s.submission_date
