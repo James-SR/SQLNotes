a2ddc5d5fc08b518299dc80b217932183ce5e333
James Solomon-Rounce <James-SR@users.noreply.github.com>
2024-07-01 18:23:23 +0100

    Update AdvancedJoins.sql

--- AdvancedJoins.sql
+++ AdvancedJoins.sql
@@ -117,3 +117,72 @@ GROUP BY submission_date;
 SELECT * 
 FROM @count_hackers;
 
+-- FINAL answer, combining the two parts together --
+
+DECLARE @everyday_hackers TABLE
+    (
+        hacker_id INT,
+        submission_date DATE
+    );
+    
+DECLARE @count_hackers TABLE
+    (
+    hacker_id INT,
+    submission_date DATE
+    );
+
+DECLARE @subdate DATE;
+DECLARE @remainingdate DATE;
+
+INSERT INTO @everyday_hackers
+SELECT hacker_ID, submission_date
+FROM submissions
+WHERE submission_date LIKE '2016-03-01';
+
+SET @subdate = '2016-03-01';
+SET @remainingdate = '2016-03-01';
+
+WHILE @subdate < '2016-03-15'
+BEGIN
+
+    SET @subdate = DATEADD(day, 1 , @subdate);
+    
+    INSERT INTO @everyday_hackers
+    SELECT s.hacker_ID, s.submission_date
+    FROM submissions s
+    JOIN @everyday_hackers eh ON eh.hacker_id = s.hacker_ID AND eh.submission_date
+        LIKE @remainingdate
+    WHERE s.submission_date LIKE @subdate;
+    
+    SET @remainingdate = DATEADD(day,1,@remainingdate);
+    
+END;
+
+INSERT INTO @count_hackers
+SELECT COUNT(DISTINCT hacker_id), submission_date
+FROM @everyday_hackers
+GROUP BY submission_date;
+
+--SELECT * 
+--FROM @count_hackers;
+
+WITH max_hacker AS (
+    SELECT 
+        ROW_NUMBER() OVER(PARTITION BY submission_date ORDER BY COUNT(s.hacker_id) DESC, s.hacker_ID ASC) AS Row#,
+        s.hacker_id, 
+        submission_date,
+        name
+    FROM submissions s
+    JOIN hackers h ON h.hacker_id = s.hacker_ID
+    GROUP BY submission_date, s.hacker_id, name
+)
+SELECT
+    s.submission_date,
+    ch.hacker_id,
+    mh.hacker_id,
+    mh.name
+FROM submissions s
+JOIN max_hacker mh ON mh.submission_date = s.submission_date AND Row# = 1
+JOIN @count_hackers ch ON ch.submission_date = s.submission_date
+GROUP BY s.submission_date, mh.hacker_id, mh.name,ch.hacker_id
+ORDER BY s.submission_date
